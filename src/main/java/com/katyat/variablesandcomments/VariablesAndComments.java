ccaee27e03ba42c60859457ccd4c77224be7def1
katexgithub <githubkate@gmail.com>
2023-05-09 21:10:03 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/com/katyat/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,46 @@
+package com.katyat.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+//        am declarat o serie de tipurile variabele
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstChartFromAscii;
+        char lastChartFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+//Initialization of the above variables
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstChartFromAscii = 100;
+        lastChartFromAscii = 'Z';
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305;
+
+        System.out.println(firstChartFromAscii);
+    }
+
+
+}
