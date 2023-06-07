f6298a532b30a2de10f57307f15c4bbfe7551b63
katexgithub <githubkate@gmail.com>
2023-06-07 19:59:40 +0300

    Added the implementation of a library system by using the collections

---
+++ src/main/java/com/katyat/bookcollectiontask/Book.java
@@ -0,0 +1,20 @@
+package com.katyat.bookcollectiontask;
+
+public class Book {
+    private String title;
+
+    private String author;
+
+    Book(String inputTitle, String inputAuthor) {
+        this.title = inputTitle;
+        this.author = inputAuthor;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+}
